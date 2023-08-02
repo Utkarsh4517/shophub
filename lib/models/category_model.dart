36a5c730a75eeccf629654269d98f74cd4e2fb69
Utkarsh4517 <uttkarsh.dav@gmail.com>
2023-08-02 23:07:47 +0530

    created category_model.dart

---
+++ lib/models/category_model.dart
@@ -0,0 +1,14 @@
+import 'package:equatable/equatable.dart';
+
+class Category extends Equatable {
+  final String name;
+  final String imageUrl;
+
+  const Category({
+    required this.name,
+    required this.imageUrl,
+  });
+
+  @override
+  List<Object?> get props => [name, imageUrl];
+}
