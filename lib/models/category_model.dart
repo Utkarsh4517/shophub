e606001132cad862e933ec9b51a60902516b2ff3
Utkarsh4517 <uttkarsh.dav@gmail.com>
2023-08-02 23:14:02 +0530

    added products in category_model.dart

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -11,4 +11,22 @@ class Category extends Equatable {
 
   @override
   List<Object?> get props => [name, imageUrl];
+
+  static List<Category> categories = [
+    const Category(
+      name: 'Soft drinks',
+      imageUrl:
+          'https://images.unsplash.com/photo-1527960471264-932f39eb5846?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+    ),
+    const Category(
+      name: 'Smoothies',
+      imageUrl:
+          'https://images.unsplash.com/photo-1622597467836-f3285f2131b8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1632&q=80',
+    ),
+    const Category(
+      name: 'Water',
+      imageUrl:
+          'https://images.unsplash.com/photo-1550505095-81378a674395?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+    )
+  ];
 }
